6e7966e6baf04f0e6168e96604d73f246534b6d1
Marcin Chlewicki <chlewicki@gmail.com>
2019-04-28 17:09:48 +0200

    refactor, healthbar colors, attacks limit

--- js/app.js
+++ js/app.js
@@ -8,97 +8,45 @@ new Vue({
         enemyFullHealth: 100,
         enemyCurrHealth: 100,
         myCurrHealth: 100,
-        isAttacking: false,
+        isAttacking: '',
+        isEnemyAttacking: '',
         enemyTurn: false,
+        enemySpecials: 10,
+        enemyHeals: 5,
+        mySpecials: 10,
+        myHeals: 5,
         logs: []
     },
     methods: {
         startGame: function () {
             this.myCurrHealth = 100;
             this.enemyCurrHealth = 100;
-            this.isAttacking = false;
+            this.isAttacking = '';
+            this.isEnemyAttacking = '';
             this.enemyTurn = false;
             this.freeze = false;
+            this.enemySpecials = 10;
+            this.enemyHeals = 5;
+            this.mySpecials = 10;
+            this.myHeals = 5;
             this.logs = [];
         },
         attack: function () {
             this.disableButtons();
             this.logs.unshift({ user: this.me, attack: 'attack' });
-            var rand = Math.floor(Math.random() * 5) + 1;
-            var vm = this;
-            var after = this.enemyCurrHealth - rand;
-            var attack = setInterval(function () {
-                if (vm.enemyCurrHealth <= 0) {
-                    clearInterval(attack);
-                    alert('you win');
-                    vm.freeze = true;
-                    vm.enableButtons();
-                } else {
-                    if (vm.enemyCurrHealth > after) {
-                        vm.isAttacking = true;
-                        vm.enemyCurrHealth--;
-                    } else {
-                        vm.isAttacking = false;
-                        clearInterval(attack);
-                        vm.enemyTurn = true;
-                    }
-                }
-            }, 50);
-            var check = setInterval(function () {
-                if (vm.isAttacking == true) {
-                    vm.move();
-                    clearInterval(check);
-                    var move = setInterval(function () {
-                        if (vm.isAttacking == false) {
-                            vm.stopMove();
-                            clearInterval(move);
-                        }
-                    }, 50);
-                }
-            }, 50);
+            var after = this.enemyCurrHealth - this.calcDamage(3, 7);
+            this.attackModel('enemy', after);
         },
         specialAttack: function () {
             this.disableButtons();
-            var rand = Math.floor(Math.random() * 10) + 1;
-            var rand2;
-            if (rand <= 5) {
-                rand2 = Math.floor(Math.random() * 2) + 1;
-                this.logs.unshift({ user: this.me, attack: 'special attack, but it failed' });
+            if (this.mySpecials > 0) {
+                var after = this.enemyCurrHealth - this.calcSpecial(this.me);
+                this.attackModel('enemy', after);
+                this.mySpecials--;
             } else {
-                rand2 = Math.floor(Math.random() * 15) + 5;
-                this.logs.unshift({ user: this.me, attack: 'special attack and it was succesfull' });
+                this.enableButtons();
             }
-            var vm = this;
-            var after = this.enemyCurrHealth - rand2;
-            var attack = setInterval(function () {
-                if (vm.enemyCurrHealth <= 0) {
-                    clearInterval(attack);
-                    alert('you win');
-                    vm.freeze = true;
-                    vm.enableButtons();
-                } else {
-                    if (vm.enemyCurrHealth > after) {
-                        vm.isAttacking = true;
-                        vm.enemyCurrHealth--;
-                    } else {
-                        vm.isAttacking = false;
-                        clearInterval(attack);
-                        vm.enemyTurn = true;
-                    }
-                }
-            }, 50);
-            var check = setInterval(function () {
-                if (vm.isAttacking == true) {
-                    vm.move();
-                    clearInterval(check);
-                    var move = setInterval(function () {
-                        if (vm.isAttacking == false) {
-                            vm.stopMove();
-                            clearInterval(move);
-                        }
-                    }, 50);
-                }
-            }, 50);
+
         },
         heal: function () {
             this.disableButtons();
@@ -106,30 +54,18 @@ new Vue({
                 alert('you are fully healed!');
                 this.enableButtons();
             } else {
-                this.logs.unshift({ user: this.me, attack: 'heal' });
-                var rand = Math.floor(Math.random() * 5) + 1;
-                var vm = this;
-                var after = this.myCurrHealth + rand;
-                var heal = setInterval(function () {
-                    if (vm.myCurrHealth >= vm.myFullHealth) {
-                        vm.myCurrHealth = vm.myFullHealth;
-                        clearInterval(heal);
-                        vm.enemyTurn = true;
-                    } else {
-                        if (vm.myCurrHealth < after) {
-                            vm.isAttacking = true;
-                            vm.myCurrHealth++;
-                        } else {
-                            vm.isAttacking = false;
-                            clearInterval(heal);
-                            vm.enemyTurn = true;
-                        }
-                    }
-                }, 50);
+                if (this.myHeals > 0) {
+                    this.logs.unshift({ user: this.me, attack: 'heal' });
+                    var after = this.myCurrHealth + this.calcDamage(7, 20);
+                    this.healModel('me', after);
+                    this.myHeals--;
+                    return;
+                }
+                this.enableButtons();
             }
         },
-        enemyChoose: function () {
-            var rand = Math.floor(Math.random() * 3) + 1;
+        enemyMove: function () {
+            var rand = this.calcDamage(1, 3);
             if (rand == 1) {
                 this.enemyAttack();
             } else if (rand == 2) {
@@ -139,93 +75,38 @@ new Vue({
             }
         },
         enemyAttack: function () {
-            var rand = Math.floor(Math.random() * 10) + 1;
-            var vm = this;
-            var after = this.myCurrHealth - rand;
+            var after = this.myCurrHealth - this.calcDamage(3, 12);
             this.logs.unshift({ user: this.enemy, attack: 'attack' });
-            var attack = setInterval(function () {
-                if (vm.myCurrHealth <= 0) {
-                    clearInterval(attack);
-                    alert('you loose');
-                    vm.freeze = true;
-                    vm.enableButtons();
-                } else {
-                    if (vm.myCurrHealth > after) {
-                        vm.isEnemyAttacking = true;
-                        vm.myCurrHealth--;
-                    } else {
-                        vm.isEnemyAttacking = false;
-                        clearInterval(attack);
-                        vm.enemyTurn = false;
-                        vm.enableButtons();
-                    }
-                }
-            }, 50);
-            var check = setInterval(function () {
-                if (vm.isEnemyAttacking == true) {
-                    vm.enemyMove();
-                    clearInterval(check);
-                    var move = setInterval(function () {
-                        if (vm.isEnemyAttacking == false) {
-                            vm.stopEnemyMove();
-                            clearInterval(move);
-                        }
-                    }, 50);
-                }
-            }, 50);
+            this.attackModel('me', after);
         },
         enemySpecialAttack: function () {
-            var rand = Math.floor(Math.random() * 10) + 1;
-            var rand2;
-            if (rand <= 5) {
-                rand2 = Math.floor(Math.random() * 2) + 1;
-                this.logs.unshift({ user: this.enemy, attack: 'special attack, but it failed' });
+            if (this.enemySpecials > 0) {
+                var after = this.myCurrHealth - this.calcSpecial(this.enemy);
+                this.attackModel('me', after);
+                this.enemySpecials--;
             } else {
-                rand2 = Math.floor(Math.random() * 15) + 5;
-                this.logs.unshift({ user: this.enemy, attack: 'special attack and it was successfull' });
+                this.enemyMove();
             }
-            var vm = this;
-            var after = this.myCurrHealth - rand2;
-            var attack = setInterval(function () {
-                if (vm.myCurrHealth <= 0) {
-                    clearInterval(attack);
-                    alert('you loose');
-                    vm.freeze = true;
-                    vm.enableButtons();
-                } else {
-                    if (vm.myCurrHealth > after) {
-                        vm.isEnemyAttacking = true;
-                        vm.myCurrHealth--;
-                    } else {
-                        vm.isEnemyAttacking = false;
-                        clearInterval(attack);
-                        vm.enemyTurn = false;
-                        vm.enableButtons();
-                    }
-                }
-            }, 50);
-            var check = setInterval(function () {
-                if (vm.isEnemyAttacking == true) {
-                    vm.enemyMove();
-                    clearInterval(check);
-                    var move = setInterval(function () {
-                        if (vm.isEnemyAttacking == false) {
-                            vm.stopEnemyMove();
-                            clearInterval(move);
-                        }
-                    }, 50);
-                }
-            }, 50);
+
         },
         enemyHeal: function () {
             if (this.enemyCurrHealth == this.enemyFullHealth) {
                 this.enemyMove();
             } else {
-                this.logs.unshift({ user: this.enemy, attack: 'heal' });
-                var rand = Math.floor(Math.random() * 5) + 1;
-                var vm = this;
-                var after = this.enemyCurrHealth + rand;
-                var heal = setInterval(function () {
+                if (this.enemyHeals > 0) {
+                    this.logs.unshift({ user: this.enemy, attack: 'heal' });
+                    var after = this.enemyCurrHealth + this.calcDamage(3, 10);
+                    this.healModel('enemy', after);
+                    this.enemyHeals--;
+                } else {
+                    this.enemyMove();
+                }
+            }
+        },
+        healModel: function (player, after) {
+            var vm = this;
+            var heal = setInterval(function () {
+                if (player == 'enemy') {
                     if (vm.enemyCurrHealth >= vm.enemyFullHealth) {
                         vm.enemyCurrHealth = vm.enemyFullHealth;
                         clearInterval(heal);
@@ -233,29 +114,72 @@ new Vue({
                         vm.enableButtons();
                     } else {
                         if (vm.enemyCurrHealth < after) {
-                            vm.isEnemyAttacking = true;
+                            vm.isEnemyAttacking = '20px';
                             vm.enemyCurrHealth++;
                         } else {
-                            vm.isEnemyAttacking = false;
+                            vm.isEnemyAttacking = 0;
                             clearInterval(heal);
                             vm.enemyTurn = false;
                             vm.enableButtons();
                         }
                     }
-                }, 50);
-            }
+                } else {
+                    if (vm.myCurrHealth >= vm.myFullHealth) {
+                        vm.myCurrHealth = vm.myFullHealth;
+                        clearInterval(heal);
+                        vm.enemyTurn = true;
+                    } else {
+                        if (vm.myCurrHealth < after) {
+                            vm.isAttacking = '20px';
+                            vm.myCurrHealth++;
+                        } else {
+                            vm.isAttacking = 0;
+                            clearInterval(heal);
+                            vm.enemyTurn = true;
+                        }
+                    }
+                }
+            }, 50);
         },
-        move: function () {
-            document.querySelectorAll('.characterimage')[1].style.marginLeft = '20px';
-        },
-        stopMove: function () {
-            document.querySelectorAll('.characterimage')[1].style.marginLeft = '0';
-        },
-        enemyMove: function () {
-            document.querySelectorAll('.characterimage')[0].style.marginRight = '20px';
-        },
-        stopEnemyMove: function () {
-            document.querySelectorAll('.characterimage')[0].style.marginRight = '0';
+        attackModel: function (enemy, after) {
+            var vm = this;
+            var attack = setInterval(function () {
+                if (enemy == 'enemy') {
+                    if (vm.enemyCurrHealth <= 0) {
+                        clearInterval(attack);
+                        alert('you win');
+                        vm.freeze = true;
+                        vm.enableButtons();
+                    } else {
+                        if (vm.enemyCurrHealth > after) {
+                            vm.isAttacking = '20px';
+                            vm.enemyCurrHealth--;
+                        } else {
+                            vm.isAttacking = 0;
+                            clearInterval(attack);
+                            vm.enemyTurn = true;
+                        }
+                    }
+                } else {
+                    if (vm.myCurrHealth <= 0) {
+                        clearInterval(attack);
+                        alert('you loose');
+                        vm.freeze = true;
+                        vm.enableButtons();
+                    } else {
+                        if (vm.myCurrHealth > after) {
+                            vm.isEnemyAttacking = '20px';
+                            vm.myCurrHealth--;
+                        } else {
+                            vm.isEnemyAttacking = 0;
+                            clearInterval(attack);
+                            vm.enemyTurn = false;
+                            vm.enableButtons();
+                        }
+                    }
+                }
+            }, 50);
+
         },
         disableButtons: function () {
             document.querySelectorAll('button').forEach((button) => {
@@ -265,13 +189,33 @@ new Vue({
         enableButtons: function () {
             document.querySelectorAll('button').forEach((button) => {
                 button.disabled = false;
-            })
+            });
+            if (this.myHeals <= 0) {
+                document.querySelector('#heal').disabled = true;
+            }
+            if (this.mySpecials <= 0) {
+                document.querySelector('#specialattack').disabled = true;
+            }
+        },
+        calcDamage: function (min, max) {
+            return Math.floor(Math.random() * max) + min;
+        },
+        calcSpecial: function (player) {
+            var rand = this.calcDamage(1, 10);
+            if (rand <= 5) {
+                rand = this.calcDamage(1, 2);
+                this.logs.unshift({ user: player, attack: 'special attack, but it failed' });
+            } else {
+                rand = this.calcDamage(5, 15);
+                this.logs.unshift({ user: player, attack: 'special attack and it was successfull' });
+            }
+            return rand;
         }
     },
     watch: {
         enemyTurn: function (value) {
             if (value == true) {
-                this.enemyChoose();
+                this.enemyMove();
             } else {
                 this.enableButtons();
             }
